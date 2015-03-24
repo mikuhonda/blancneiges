e213ad0e1ee8e6a73766813ae1b40bf7b980a4c8
mikuhonda <mikuri419mikuri@gmail.com>
2015-03-24 16:52:18 +0900

    Add all

---
+++ js/page_scroll.js
@@ -0,0 +1,13 @@
+$(function(){
+    var url = $(location).attr('href');
+    if (url.indexOf("?id=") == -1) {
+        // スムーズスクロール以外の処理（必要なら）
+    }else{
+        // スムーズスクロールの処理
+        var url_sp = url.split("?id=");
+        var hash   = '#' + url_sp[url_sp.length - 1];
+        var tgt    = $(hash);
+        var pos    = tgt.offset().top;
+        $("html, body").animate({scrollTop:pos}, 2000, "swing");
+    }
+});
