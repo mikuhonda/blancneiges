810ea6375c7c4790c3938a091280683e3cebd738
mikuhonda <mikuri419mikuri@gmail.com>
2015-03-25 12:00:58 +0900

    Nomessage

--- js/page_scroll.js
+++ js/page_scroll.js
@@ -8,6 +8,6 @@ $(function(){
         var hash   = '#' + url_sp[url_sp.length - 1];
         var tgt    = $(hash);
         var pos    = tgt.offset().top;
-        $("html, body").animate({scrollTop:pos}, 2000, "swing");
+        $("html, body").animate({scrollTop:pos}, 1000, "swing");
     }
 });
