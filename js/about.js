50571846f252ee906d997d90dbaeb8e39c10b07c
mikuhonda <mikuri419mikuri@gmail.com>
2015-04-12 14:25:04 +0900

    Modify sp

--- js/about.js
+++ js/about.js
@@ -1,23 +1,15 @@
-$(function () {
-  $('span').click(function() { 
-    if ($(this).attr('class') == 'selected') {
-      // メニュー非表示
-      $(this).removeClass('selected').next('ul').slideUp('fast');
-    } else {
-      // 表示しているメニューを閉じる
-      $('span').removeClass('selected');
-      $('ul').hide();
-
-      // メニュー表示
-      $(this).addClass('selected').next('ul').slideDown('fast');
-    }    
-  });
-  
-  // メニュー領域外をクリックしたらメニューを閉じる
-  $('body').click(function() {
-    if (over_flg == false) {
-      $('span').removeClass('selected');
-      $('ul').slideUp('fast');
-    }
-  });
+$(function(){
+     
+$(".accordion p").click(function(){
+    $(this).next("ul").slideToggle();
+    $(this).children("span").toggleClass("open");
+}); 
+ 
+$(".accordion dt").click(function(){
+    $(this).next("dd").slideToggle();
+    $(this).next("dd").siblings("dd").slideUp();
+    $(this).toggleClass("open");    
+    $(this).siblings("dt").removeClass("open");
+});
+ 
 });
